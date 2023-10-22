81510b71ab95d7023b74a5ce33b1facadc1e8d7d
tardis5356 <ftctardis@gmail.com>
2023-10-22 15:49:33 -0400

    10/22/2023. Includes code for Arm subsystem for the Optimus commandbase program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBots/optimus/subsystems/Arm.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.DemoBots.optimus.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+
+public class Arm extends SubsystemBase {
+    private DcMotor AMotor;
+    private TouchSensor MSense;
+}
