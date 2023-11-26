75119f326d7577f58cbdbeb928ed2bcac3880248
Eavan <eavanqin0716@gmail.com>
2023-11-26 13:28:09 -0500

    11/26/2023

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageTestBed2023_2024/CSTB_AutoTrajectories_RedWingsFull.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+public class CSTB_AutoTrajectories_RedWingsFull {
+
+    //BLUE BACKSTAGE
+    public static final Pose2d blueBackstage_StartPos = new Pose2d(-8, 64.5, Math.toRadians(90));
+    public static final Pose2d blueBackstage_WaypointParkPos = new Pose2d(-61.5, 13.5, Math.toRadians(180));
+    public static final Pose2d blueBackstage_ParkedPos = new Pose2d(-63, 63, Math.toRadians(220));
+
+    //BLUE WINGS
+    public static final Pose2d blueWings_StartPos = new Pose2d(36, -64.5, Math.toRadians(90));
+    public static final Pose2d blueWings_dropPurple = new Pose2d(-61.5, 13.5, Math.toRadians(270));
+    public static final Pose2d blueWings_ParkedPos = new Pose2d(-63, 63, Math.toRadians(270));
+
+    //RED BACKSTAGE
+    public static final Pose2d redBackstage_StartPos = new Pose2d(-8, 64.5, Math.toRadians(270));
+    public static final Pose2d redBackstage_ParkedPos = new Pose2d(-61.5, 64.5, Math.toRadians(270));
+    public static final Pose2d redBackstage_ToBackdrop = new Pose2d(-63, -63, Math.toRadians(270));
+
+    //RED WINGS
+    public static final Pose2d redWings_StartPos = new Pose2d(36, 64.5, Math.toRadians(270));
+    public static final Pose2d redWings_PostPurplePos = new Pose2d(40, 16, Math.toRadians(270));
+    public static final Pose2d redWings_ParkedPos = new Pose2d(-63, -63, Math.toRadians(270));
+
+    public static TrajectorySequence blueBackstage_ToDecisionPoint;
+    public static TrajectorySequence blueWings_ToDecisionPoint , blueWings_ToMiddlePark;
+    public static TrajectorySequence redBackstage_ToDecisionPoint , redBackstage_ToParkedPos;
+    public static TrajectorySequence redWings_ToDecisionPoint , redWings_ToMiddlePark , redWings_ToCenterSpike, redWings_ToLeftSpike , redWings_ToRightSpike;
+
+    public static void generateTrajectories() {
+        generateTrajectories(null);
+    }
+
+    public static void generateTrajectories(CSTB_SampleMecanumDrive drive) {
+
+        class Scratch {
+            public void main(String[] args) {
+                redBackstage_ToParkedPos =
+                        drive.trajectorySequenceBuilder(redBackstage_StartPos)
+                                .setReversed(true)
+                                .lineTo(new Vector2d(-50, 64.5), CSTB_SampleMecanumDrive.getVelocityConstraint(86, CSTB_DriveConstants.MAX_ANG_VEL, CSTB_DriveConstants.TRACK_WIDTH),
+                                        CSTB_SampleMecanumDrive.getAccelerationConstraint(CSTB_DriveConstants.MAX_ACCEL))
+                                .setReversed(false)
+                                .build();
+
+
+                //redWings
+                redWings_ToLeftSpike = //shift and go forward to score center
+                        drive.trajectorySequenceBuilder(redWings_ToDecisionPoint.end())
+                                .splineToConstantHeading(new Vector2d(-42, 22), Math.toRadians(0))
+                                .build();
+
+                redWings_ToCenterSpike = //shift and go forward to score center
+                        drive.trajectorySequenceBuilder(redWings_ToDecisionPoint.end())
+                                .splineToConstantHeading(new Vector2d(-40,12.01), Math.toRadians(0))
+                                .build();
+
+                redWings_ToRightSpike = //shift and go forward to score center
+                        drive.trajectorySequenceBuilder(redWings_ToDecisionPoint.end())
+                                .splineToConstantHeading(new Vector2d(-38, 26), Math.toRadians(0))
+                                .build();
+
+                redWings_ToMiddlePark = //go to and spin to parking between backdrops
+                        drive.trajectorySequenceBuilder(redWings_ToDecisionPoint.end())
+                                .splineToConstantHeading(new Vector2d( -42, 16), Math.toRadians(0))
+                                .build();
+            }
+        }}}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageTestBed2023_2024/CSTB_redWings_Full.java
@@ -0,0 +1,131 @@
+package org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024;
+
+import static org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024.CSTB_AutoTrajectories_RedWingsFull.redWings_ToCenterSpike;
+import static org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024.CSTB_AutoTrajectories_RedWingsFull.redWings_ToDecisionPoint;
+import static org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024.CSTB_AutoTrajectories_RedWingsFull.redWings_ToLeftSpike;
+import static org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024.CSTB_AutoTrajectories_RedWingsFull.redWings_ToMiddlePark;
+import static org.firstinspires.ftc.teamcode.CenterStageTestBed2023_2024.CSTB_AutoTrajectories_RedWingsFull.redWings_ToRightSpike;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.vision.visionTesting.RedPropDetection;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+import java.util.concurrent.TimeUnit;
+
+//public class CSTB_redWings_Full {
+
+
+@Autonomous(group = "drive", name = "CSTB red wings Full")
+public class CSTB_redWings_Full extends CommandOpMode {
+    ElapsedTime runtime = new ElapsedTime();
+
+    private CSTB_SampleMecanumDrive drive;
+    private VisionPortal portal;
+    private RedPropDetection redPropThreshold;
+    //    private Lift lift;
+    private TrajectorySequence pixelTrajectoryRight, pixelTrajectoryMiddle, pixelTrajectoryLeft, redWings_parkTrajectory;
+    private FtcDashboard dashboard = FtcDashboard.getInstance();
+
+    @Override
+    public void initialize() {
+//        MultipleTelemetry telemetry2 = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        ////////‼️‼️⁉️⁉️CAMERA INITIALIZATION/DEFINING ⁉️⁉️⁉️
+//        public void runOpMode () throw InterruptedException {
+        redPropThreshold = new RedPropDetection();
+        portal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+//                .addProcessor(redPropThreshold)
+                .setCameraResolution(new Size(640, 480))
+//                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
+//                .enableLiveView(true)
+//                .setCamera()
+                .addProcessor(redPropThreshold)
+                .build();
+
+        if (portal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (portal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        ExposureControl exposureControl = portal.getCameraControl(ExposureControl.class);
+        GainControl gainControl = portal.getCameraControl(GainControl.class);
+
+        boolean wasExposureSet = exposureControl.setMode(ExposureControl.Mode.Manual);
+        exposureControl.setExposure(50, TimeUnit.MILLISECONDS);
+        gainControl.setGain(0);
+
+//      everything above is included in the previous comment, this order needs to be maintained.
+
+//            waitForStart();
+//        }
+
+        drive = new CSTB_SampleMecanumDrive(hardwareMap);
+
+
+        drive.setPoseEstimate(CSTB_AutoTrajectories_RedWingsFull.redWings_StartPos);
+        CSTB_AutoTrajectories_RedWingsFull.generateTrajectories(drive);
+
+
+//gripper.close();
+////////////////////////////DEFINING PARK TRAJECTORIES//////////////////////////////
+
+
+////////////////////////////////////DONE DEFINING PARK TRAJECTORIES///////////////////////////////////////
+
+
+        switch (redPropThreshold.getPropPosition()) {
+            case "left":
+                redWings_parkTrajectory = redWings_ToLeftSpike;
+                telemetry.addLine("park traj 1");
+                break;
+            case "center":
+                redWings_parkTrajectory = redWings_ToCenterSpike;
+                telemetry.addLine("park traj 2");
+                break;
+            case "right":
+                redWings_parkTrajectory = redWings_ToRightSpike;
+                telemetry.addLine("park traj 3");
+                break;
+        }
+
+        telemetry.setMsTransmissionInterval(50);
+
+        while (!isStarted() && !isStopRequested()) {
+
+            telemetry.addLine("waitForStart");
+            telemetry.update();
+            sleep(20);
+        }
+
+
+        telemetry.update();
+        schedule(new SequentialCommandGroup(
+                new CSTB_FollowTrajectoryCommand(drive, redWings_ToDecisionPoint),
+                new CSTB_FollowTrajectoryCommand(drive, redWings_parkTrajectory),
+                new WaitCommand(500),
+                new CSTB_FollowTrajectoryCommand(drive, redWings_ToMiddlePark)
+
+        ));
+
+    }
+}
