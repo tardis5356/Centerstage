ed662de06cae8d5667e44d6084cd1455dda0fd2b
thesimg <graham.simons05@gmail.com>
2023-12-10 16:57:33 -0500

    2023-12-10 IntakeOutSlowCommand

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARTEMIS/commands/IntakeOutSlowCommand.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.ARTEMIS.commands;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.ARTEMIS.subsystems.Intake;
+
+public class IntakeOutSlowCommand extends SequentialCommandGroup {
+    public IntakeOutSlowCommand(Intake intake) {
+        addCommands(
+                new InstantCommand(intake::slowOut),
+                new WaitCommand(1000),
+                new InstantCommand(intake::stop)
+        );
+    }
+}
